d728ecae66e117b4cc6f420b15ce609318db87d7
Ingyu Woo <iglenwoo@gmail.com>
2019-11-22 12:35:36 -0800

    WIP: add signup test

---
+++ scenarios/osused-store/__mock__.js
@@ -0,0 +1,13 @@
+const URL = 'http://localhost:3000'
+
+const mockNewUser = {
+  email: 'aaa@aaa.com',
+  password: '12345678',
+  firstName: 'First',
+  lastName: 'Last'
+}
+
+module.exports = {
+  URL,
+  mockNewUser,
+}
--- scenarios/osused-store/index.js
+++ scenarios/osused-store/index.js
@@ -1,36 +1,58 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
-
-const BASE_URL = 'http://localhost'
-const BASE_PORT = '3000'
-const URL = `${BASE_URL}:${BASE_PORT}`
+const { URL, mockNewUser } = require('./__mock__')
 
 async function testOsusedStore() {
   const driver = await new Builder().forBrowser('chrome').build();
   try {
     // Loading home page
     await driver.get(URL);
-    console.info('1. loaded url: ', await driver.getCurrentUrl())
-    const loginLinkButton = await driver.findElement(By.xpath('//span[text()=\'Login\']'));
-    loginLinkButton.click()
+    console.info('loaded url: ', await driver.getCurrentUrl())
+    
+    //TODO: SIGN UP:
+    await driver.findElement(By.xpath('//span[text()=\'Login\']')).click()
+    console.info('1. login clicked')
+    await driver.wait(until.urlIs(`${URL}/login`));
+    console.info('2. current url: ', await driver.getCurrentUrl())
+    await driver.findElement(By.xpath('//a[@href=\'/sign-up\']')).click()
+    console.info('3. sign up clicked')
+    await driver.wait(until.urlIs(`${URL}/sign-up`));
+    console.info('4. current url: ', await driver.getCurrentUrl())
 
-    // LOGIN
-    console.info('2. login clicked')
-    driver.wait(until.urlIs(`${URL}/login`));
-    console.info('3. current url: ', await driver.getCurrentUrl())
-    const emailInput = await driver.findElement(By.id('email'))
-    console.info('4. got email input')
-    emailInput.sendKeys('qwe@qwe.com') // todo: extract this hardcoded value, then make a mock data, then signup with it before login
-    const passwordInput = await driver.findElement(By.id('password'))
-    console.info('5. got password input')
-    passwordInput.sendKeys('qweqweqwe') // todo: extract this hardcoded value, then make a mock data, then signup with it before login
-    const loginButton = await driver.findElement(By.xpath('//button[@type=\'submit\']'))
-    console.info('6. got login button')
-    loginButton.click()
-    console.info('7. clicked login button')
-    driver.wait(until.urlIs(`${URL}/items`));
+    await driver.findElement(By.id('firstName')).sendKeys(mockNewUser.firstName)
+    await driver.findElement(By.id('lastName')).sendKeys(mockNewUser.lastName)
+    await driver.findElement(By.id('email')).sendKeys(mockNewUser.email)
+    await driver.findElement(By.id('password')).sendKeys(mockNewUser.password)
+    await driver.findElement(By.id('Checkpassword')).sendKeys(mockNewUser.password)
+    console.info('5. set user')
+
+    await driver.findElement(By.xpath('//button[@type=\'submit\']')).click()
+    console.info('6. clicked sign up')
+
+    //TODO: fix alert acceptance
+    await driver.switchTo().alert().accept();
+    console.info('7. clicked alert')
+
+    await driver.wait(until.urlIs(`${URL}/items`));
     console.info('8. current url: ', await driver.getCurrentUrl())
 
-    //TODO: SIGN UP:
+
+    // LOGIN
+    // console.info('2. login clicked')
+    // driver.wait(until.urlIs(`${URL}/login`));
+    // console.info('3. current url: ', await driver.getCurrentUrl())
+    // const emailInput = await driver.findElement(By.id('email'))
+    // console.info('4. got email input')
+    // emailInput.sendKeys('qwe@qwe.com') // todo: extract this hardcoded value, then make a mock data, then signup with it before login
+    // const passwordInput = await driver.findElement(By.id('password'))
+    // console.info('5. got password input')
+    // passwordInput.sendKeys('qweqweqwe') // todo: extract this hardcoded value, then make a mock data, then signup with it before login
+    // const loginButton = await driver.findElement(By.xpath('//button[@type=\'submit\']'))
+    // console.info('6. got login button')
+    // loginButton.click()
+    // console.info('7. clicked login button')
+    // driver.wait(until.urlIs(`${URL}/items`));
+    // console.info('8. current url: ', await driver.getCurrentUrl())
+
 
     //TODO: ADD: add an item -> check if the item is in the list
 

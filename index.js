a348093751fd4bca54f17ea98cb9ec9650501e12
Ingyu Woo <iglenwoo@gmail.com>
2019-11-25 16:42:18 -0800

    remove unused code

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 var chrome = require('selenium-webdriver/chrome');
 chrome.setDefaultService(new chrome.ServiceBuilder(require('chromedriver').path).build());
-const {example} = require('./scenarios/google')
 const {signUp, login} = require('./scenarios/users')
 const {testOsusedItems} = require('./scenarios/items')
-// function getFunctions() {
-//     ''
-// }
 
 try {
-    // example()
     signUp().then(e => {
         console.log(e)
         login().then(e => {
@@ -19,21 +14,6 @@ try {
     testOsusedItems().then(e => {
         console.log(e)
     }).catch(e => console.error(e));
-    // functions = getFunctions()
-    // for fname in fucntions:
-    //     const fn = new function(fname);
-    //     fn.start()
 } catch (e) {
     console.log(e)
 }
-
-// (async function example() {
-//     const driver = await new Builder().forBrowser('chrome').build();
-//     try {
-//         await driver.get('http://ec2-18-191-174-133.us-east-2.compute.amazonaws.com:3000/items');
-//         await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-//         await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
-//     } finally {
-//         await driver.quit();
-//     }
-// })();
